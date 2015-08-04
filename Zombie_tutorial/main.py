30274121620fc4860ad794e7a64f8a374d02916b
John Taylor <taylorjohn@users.noreply.github.com>
2015-08-03 23:34:19 -0400

    updated main.py working in python 3.4
    updated main.py working in python 3.4

--- Zombie_tutorial/main.py
+++ Zombie_tutorial/main.py
@@ -1,14 +1,13 @@
 #   Things that neeeded to be imported
 import pygame
-from Funk import *
-#   file that contains the tile class
+import sys
+import Funk
 from tileC import Tile
-#   file that contains the object class
 from object_classes import *
-#   file that contains the event and interaction class
 from interaction import interaction
-#   file that contains the A star algo for enemy pathfinding
 from A_Star import A_Star
+from time import sleep
+
 
 #   Before you can do much with pygame, you will need to initialize it
 pygame.init()
@@ -17,11 +16,11 @@ pygame.font.init()
 #   initialize the mixer module
 pygame.mixer.init()
 #   Load a music file for playback
-pygame.mixer.music.load('./audio/zombie_theme.ogg')
+pygame.mixer.music.load('audio/zombie_theme.ogg')
 #   Start the playback of the music stream
 pygame.mixer.music.play(-1)
 #   Initialize a window or screen for display
-screen = pygame.display.set_mode((704, 448))  # 32, 32 tiles are 32 by 32
+screen = pygame.display.set_mode((704, 448))    # 32, 32 tiles are 32 by 32
 
 # pre start up tiles?
 Tile.pre_init(screen)
@@ -33,9 +32,10 @@ FPS = 20
 # keep count of frames
 total_frames = 0
 # loads background image dungeon
-dungeon = pygame.image.load('dungeon.jpg')
+dungeon = pygame.image.load('images/dungeon.jpg')
 # the player image called survivor 32x32 pixels
-survivor = Survivor(32*2, 32*4)
+survivor = Survivor(32 * 2, 32 * 4)
+
 # begins the while true loop
 while True:
     # screen.blit(sourceSurface, destinationRect, optionalSourceRect)
@@ -43,28 +43,75 @@ while True:
     screen.blit(dungeon, (0, 0))
     #   calls the zombie character object class spawn
     Zombie.spawn(total_frames, FPS)
-
-    #   calls the zombie character object class movement
-    Zombie.movement()
-
+    #   updates the screen
+    Zombie.update(screen, survivor)
     #   calls the surivor or player character object class movement
     survivor.movement()
-
     # bullet stop after hitting zombie or going off screen
     Bullet.super_massive_jumbo_loop(screen)
     # a star algo to screen, player, total frames and frames per sec
     A_Star(screen, survivor, total_frames, FPS)
-
     # passes the interaction to the screen, player
     interaction(screen, survivor)
-
     # draws the survivor or player to the screen
     survivor.draw(screen)
-    # draws the zombies to te screen
-    Zombie.draw_zombies(screen)
+    # draws the text to screen for the score
+    Funk.text_to_screen(screen, 'Health: {0}'.format(survivor.health), 0, 0)
     #   Update the full display Surface to the screen
     pygame.display.flip()
     # the clock tick is equal to the FPS frames per second 20
     clock.tick(FPS)
     # Add one to the total frames
     total_frames += 1
+    # when you die sleep for 2.5 sec and dead picture
+    if survivor.health <= 0:
+        sleep(2.5)
+        screen.blit(pygame.image.load('images/dead.jpg'), (0, 0))
+        pygame.display.update()
+        break
+#sleep for 4 ticks
+sleep(4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
